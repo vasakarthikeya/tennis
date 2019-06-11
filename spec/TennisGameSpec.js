16fb4a3da88ca24e2d22adc3993220a45d03e428
2019-Q2-SOW-DEV-030 <vasakarthikeya@gmail.com>
2019-06-11 13:35:51 +0200

    Refactor game initialization into beforeEach function

--- spec/TennisGameSpec.js
+++ spec/TennisGameSpec.js
@@ -1,9 +1,13 @@
 var TennisGame = require("../src/TennisGame");
 
 describe('TennisGame', function () {
+    var tennisGame;
+
+    beforeEach(function() {
+        tennisGame= new TennisGame();
+    });
 
     it('Should get the scores of the players at match start', function () {
-        var tennisGame = new TennisGame();
 
         var score = tennisGame.getScore();
 
@@ -12,12 +16,11 @@ describe('TennisGame', function () {
     });
 
     it('Should player 1 win the first ball', function() {
-        var tennisGame = new TennisGame();
         
         tennisGame.playerOneScored();
 
         var score = tennisGame.getScore();
-        
+
         expect(score.player1).toEqual("15");
         expect(score.player2).toEqual("0");
     });
